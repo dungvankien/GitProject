2d4b682291a671a031a2e9e3007517e501bb74bf
dungvankien <vankienhue@gmail.com>
2022-06-14 22:25:55 +0700

    add_temary

--- javaScript/tax.js
+++ javaScript/tax.js
@@ -1,22 +1,54 @@
-var luong=5000000;
+//if else
+var salary=50000000;
 var tax;
-if(luong>80000000){
-    tax=luong*0.35;
-else if(luong>52000000) {
-        tax=luong*0.3;
-     else if(luong>32000000){
-            tax=luong*0.25;
-          else if(luong>18000000){
-                tax=luong*0.2;
-                else if(luong>10000000){
-                    tax=luong*0.1;
-                else{
-                        tax=luong*0.05;
-                    }
+// if (salary>80000000) {
+//     tax=salary*0.35;}
+// else if (salary>52000000) {
+//     tax=salary*0.3;}
+//     else if (salary>32000000) {
+//         tax=salary*0.25;}
+//         else if (salary>18000000) { 
+//             tax=salary*0.2;}
+//             else if (salary>10000000) {
+//                 tax=salary*0.15;}
+//                 else if (salary >5000000) {
+//                     tax=salary*0.1;}
+//                     else {
+//                         tax=salary*0.05;}
+// console.log('Pay tax: ' + tax)
 
-                }
-            }
-        }
-    }
-}
-console.log(tax);
+// swith--case
+// switch (true) {
+//     case (salary>80000000):
+//         tax=salary*0.35;
+//         break;
+//     case (salary>52000000):
+//         tax=salary*0.3;
+//         break;
+//     case (salary>32000000):
+//         tax=salary*0.25;
+//         break;
+//     case (salary>18000000):
+//         tax=salary*0.2;
+//         break;
+//     case (salary>10000000):
+//         tax=salary*0.15;
+//         break;
+//     case (salary>5000000):
+//         tax=salary*0.1;
+//         break;
+//     case (salary>0):
+//         tax=salary*0.05;
+//         break;
+//    }   
+// console.log( 'Pay tax: ' + tax)  
+
+// temary operator
+console.log((salary>80000000)?(salary*0.35):
+                (salary>52000000)?(salary*0.3):
+                    (salary>32000000)?(salary*0.25):
+                        (salary>18000000)?(salary*0.2):
+                            (salary>1000000)?(salary*0.15):
+                                (salary>5000000)?(salary*0.1):
+                                    (salary>0)?(salary*0.05):
+                                        (undefined));
