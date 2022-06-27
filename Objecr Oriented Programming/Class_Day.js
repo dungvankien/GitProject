a5a4c21d9875bac9b1623793b23d30755ab91d45
dungvankien <vankienhue@gmail.com>
2022-06-27 10:05:59 +0700

    addOject

---
+++ Objecr Oriented Programming/Class_Day.js
@@ -0,0 +1,40 @@
+let myDate=function(day, month, year){
+    this.day=day;
+    this.month=month;
+    this.year=year;
+    this.getDay=function(){
+        return this.day;
+    }
+    this.getMonth=function(){
+        return this.month;
+    }
+    this.getYear=function(){
+        return this.year;
+    }
+    this.setDay=function(day){
+        this.day=day;
+    }
+    this.setMonth=function(month){
+        this.month=month;
+    }
+    this.setYear=function(year){
+        this.year=year;
+    }
+    this.toString=function(){
+        return `${day}/${month}/${year}`;
+    }
+}
+let date=new myDate(2,2,2007);
+// let day=date.getDay();
+// let month=date.getMonth();
+// let year=date.getYear();
+// console.log(`${day} / ${month} / ${year}`);
+date.setDay(10);
+date.setMonth(10);
+date.setYear(2019);
+let day=date.getDay();
+let month=date.getMonth();
+let year=date.getYear();
+console.log(`${day} / ${month} / ${year}`);
+let result=date.toString();
+console.log(result);
