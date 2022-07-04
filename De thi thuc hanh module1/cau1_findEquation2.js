04cceada06cd47882d430eca34fabf25b366c3af
dungvankien <vankienhue@gmail.com>
2022-07-04 10:41:02 +0700

    addfix

--- De thi thuc hanh module1/cau1_findEquation2.js
+++ De thi thuc hanh module1/cau1_findEquation2.js
@@ -1,19 +1,22 @@
 function findEquation2(a, b, c) {
+    if (a == 0 && b == 0 && c == 0) {
+        return "Phương trình vô số nghiệm"
+    }
     if (a == 0 && b == 0) {
-        return console.log("phuong trình không xác định");
+        return "phuong trình không xác định";
     }
     else if (a == 0 && b != 0) {
-        return console.log(`Phuong trinh co 1 nghiem x= ${(-c) / b} `);
+        return `Phuong trinh co 1 nghiem x= ${(-c) / b} `;
     }
     let denta = b * b - 4 * a * c;
     if (denta < 0) {
-        return console.log("Phương trình vô nghiệm");
+        return "Phương trình vô nghiệm";
     }
     else if (denta == 0) {
-        return console.log("Phương trình có một nghiệm x= " + ((-b) / (2 * a)));
+        return "Phương trình có một nghiệm x= " + ((-b) / (2 * a));
     }
     else {
-        return console.log("Phương trình có 2 nghiệm x1= " + (-b + Math.sqrt(denta)) / (2 * a) + " Nhiệm x2= " + (-b - Math.sqrt(denta)) / (2 * a));
+        return "Phương trình có 2 nghiệm x1= " + (-b + Math.sqrt(denta)) / (2 * a) + " Nhiệm x2= " + (-b - Math.sqrt(denta)) / (2 * a);
     }
 }
 findEquation2(8, 12, -20);
--- De thi thuc hanh module1/cau2_findIndexArray.js
+++ De thi thuc hanh module1/cau2_findIndexArray.js
@@ -1,11 +1,11 @@
-function findIndexAraay(Number,array){
+function findIndexArray(Number,array){
     for(let i=0;i<array.length;i++){
         if (Number==array[i]){
             return i;
         }
     }
-    return `${Number} Không có trong mảng ${array}`
+    return `${Number} Không có trong mảng ${array}`;
 }
-let array=[1,3,4,6,7,8,9]
-let number=4;
-findIndexAraay( number,array);
+let array=[1,3,4,6,7,8,9];
+let number=Number(prompt("Nhập một số nguyên bất kỳ"));
+findIndexArray( number,array);
